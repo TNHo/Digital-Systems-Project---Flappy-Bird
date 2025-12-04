ef01bd1729f8d01c58268d25818384ac2aad94d3
TNHo <88540087+TNHo@users.noreply.github.com>
2025-12-04 14:43:23 -0500

    Update flappy_nextstate_logic.vhd
    Implement the next state logic part of schematic

--- VHDL Sources/Design Sources/Flappy/flappy_nextstate_logic.vhd
+++ VHDL Sources/Design Sources/Flappy/flappy_nextstate_logic.vhd
@@ -19,7 +19,18 @@ end flappy_nextstate_logic;
 architecture Structural of flappy_nextstate_logic is
 
 -- Componenents needed:
-
+-- and
+component two_input_and_gate is
+    Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           Q : out STD_LOGIC);
+end component;
+-- or
+component two_input_or_gate is
+    Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           Q : out STD_LOGIC);
+end component;
 
 
 
@@ -28,12 +39,34 @@ signal vdd : std_logic := '1';
 signal gnd : std_logic := '0';
 
 -- Internal signals;
-
+signal Bnot: std_logic;
+signal S1NotN: std_logic;
+signal S0NotN: std_logic;
+signal BGndNot: std_logic;
+signal WOrO1: std_logic;
+signal WOrO2: std_logic;
+signal WOrO3: std_logic;
+signal WAndOut1: std_logic;
+signal WAndOut2: std_logic;
+signal WAndOut3: std_logic;
 
 begin
 
 -- Instances of components
-
+-- Inverters
+Bnot <= not B;
+BGndNot <= not below_gnd;
+S1NotN <= not S1;
+S0NotN <= not S0;
+-- Logic of the next state
+Or1: two_input_or_gate port map(below_gnd, S1, WOrO1);
+Or2: two_input_or_gate port map(S1NotN, S0NotN, WOrO3);
+Or4: two_input_or_gate port map(Bnot, WOrO3, WOrO2);
+And1: two_input_and_gate port map(S1NotN, S0, WAndOut1);
+And2: two_input_and_gate port map(WOrO1, WOrO2, N0);
+And4: two_input_and_gate port map(Bnot, S1, WAndOut2);
+And3: two_input_and_gate port map(BGndNot, WAndOut2, WAndOut3);
+Or3: two_input_or_gate port map(WAndOut1, WAndOut3, N1);
 
 -- Single bit test port
 testport <= gnd;
